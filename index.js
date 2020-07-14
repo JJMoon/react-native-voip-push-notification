c8fc0691dad9dd9f785480810351c879ec621576
JwMoon <jm99tt@gmail.com>
2020-07-14 17:41:19 +0900

    Add bridging method

--- index.js
+++ index.js
@@ -136,6 +136,16 @@ export default class RNVoipPushNotification {
     }
 
     /**
+     * Set JS ready variable
+     *
+     * @static
+     * @memberof RNVoipPushNotification
+     */
+    static setJsReady() {
+        RNVoipPushNotificationManager.setJsReady();
+    }
+
+    /**
      * When you have processed necessary initialization for voip push, tell ios completed.
      * This is mainly for ios 11+, which apple required us to execute `complete()` when we finished.
      * If you want to use this function, make sure you call `[RNVoipPushNotificationManager addCompletionHandler:uuid completionHandler:completion];`
